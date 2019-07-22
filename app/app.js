191235eeda91908fcc3301a1251b65bcc693be63
Takuma Nagata <takuma.fn@gmail.com>
2019-07-23 01:27:11 +0900

    Add routing files.
    ルーティング関係のテストファイルを追加。

--- app/app.js
+++ app/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello, World'));
-app.listen(3000, () => console.log('listening on port 80:3000'));
+const indexRouter = require('./router/index');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', indexRouter);
+app.listen(3000);
